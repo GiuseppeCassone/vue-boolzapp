b9b38440dc84ac27529d0816071d96f8ca83765e
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-02-22 13:04:04 +0100

    realizzato struttura della colonna sinistra

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@
 // Milestone 2
 // Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
 // Click sul contatto mostra la conversazione del contatto cliccato
-
-
 // Milestone 3
 // Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
 // Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
@@ -188,5 +186,5 @@ createApp({
             
         }
     },
-    
+
 }).mount("#app");
